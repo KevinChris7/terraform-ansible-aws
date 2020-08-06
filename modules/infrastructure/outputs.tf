86b1ac8c82e131c70b501bb2401d16cd91c41f33
Kevin Christopher <kevinchristopherkc7@gmail.com>
2020-08-06 21:25:28 +0530

    Added outputs.tf file for infrastructure

---
+++ modules/infrastructure/outputs.tf
@@ -0,0 +1,30 @@
+# outputs.tf
+
+########### Infrastructure Outputs ###########
+
+output "vpc" {
+  value       = aws_vpc.primary_vpc.arn
+  description = "ARN of VPC"
+}
+
+output "vpc_cidr" {
+  value       = aws_vpc.primary_vpc.cidr_block
+  description = "CIDR Block of VPC"
+}
+
+output "vpcid" {
+  value       = aws_vpc.primary_vpc.id
+  description = "ID of VPC"
+}
+
+output "publicsubnet" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "privatesubnet1" {
+  value = aws_subnet.private_subnetA.id
+}
+
+output "privatesubnet2" {
+  value = aws_subnet.private_subnetB.id
+}
