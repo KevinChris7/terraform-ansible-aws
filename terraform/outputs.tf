8b5f1e8457dbabb1533f9982aae71ab283bc39c5
Kevin Christopher <kevinchristopherkc7@gmail.com>
2020-08-09 22:47:15 +0530

    VPC output added and description for all outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,15 +4,24 @@
 
 # Load Balancer DNS Output
 output "alb_dns" {
-  value = aws_lb.alb.dns_name
+  value       = aws_lb.alb.dns_name
+  description = "Load Balancer DNS"
 }
 
 # Webserver1 Instance 
 output "Web_Server1" {
-  value = aws_instance.webserver1.public_ip
+  value       = aws_instance.webserver1.public_ip
+  description = "Web_Server1 IP"
 }
 
 # Webserver2 Instance 
 output "Web_Server2" {
-  value = aws_instance.webserver2.public_ip
+  value       = aws_instance.webserver2.public_ip
+  description = "Web_Server2 IP"
+}
+
+# 
+output "VPC" {
+  value       = aws_security_group.web_sec_gp.vpc_id
+  description = "VPC Id"
 }
